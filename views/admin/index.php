ff32327de8aff2ccfd3a74a4f042298a60558610
viktoriya2002 <pivovarova-2002@bk.ru>
2023-02-10 14:59:22 +0300

    v3

--- views/admin/index.php
+++ views/admin/index.php
@@ -2,7 +2,9 @@
 /* @var $this yii\web\View */
 ?>
 <h1><center>Панель администратора</center></h1>
+<div><center>
 <p><a href="/product">Управление товарами</a></p>
 <p><a href="/order">Управление заказами</a></p>
-<p><a href="/category">Управление категориями</a></p>
+<p><a href="/category">Управление категориями</a></p></center>
+</div>
 
